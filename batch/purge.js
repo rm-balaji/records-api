62e8318566444ec2f89aff12663215ccc57a8339
rm-balaji <rm.balaji@gmail.com>
2023-09-23 22:30:10 +0530

    update purge logic

--- batch/purge.js
+++ batch/purge.js
@@ -2,21 +2,30 @@ const fs = require('fs');
 
 exports.purgeRecords = ()=>{
     let currentTimeStamp = Date.now();
-    let purgeWindowStart = currentTimeStamp - 1000 * 60 * 35;
+    let purgeWindowStart = currentTimeStamp - 1000 * 60 * 37;
     let purgeWindowEnd = currentTimeStamp - 1000 * 60 * 30;
-
+    purgeWindowStart = parseInt(purgeWindowStart / 1000);
+    purgeWindowEnd = parseInt(purgeWindowEnd / 1000);
     for (let i = purgeWindowStart; i < purgeWindowEnd; i++) {
         let prefix = i+ "";
         let part = prefix.substring(prefix.length - 1);
-        fs.readdirSync('./records/' + part + "/").forEach(file => {
-            console.log(file);
-            if (file.startsWith(prefix)) {
-                fileNames.push('./records/' + part + "/" + file);
-            }
-        });
+        let folder = getPartition(i * 1000);
+        if (fs.existsSync('./records/' + folder + "/" + i + ".txt")) {
+            fileNames.push('./records/' + folder + "/" + i + ".txt")
+        }
     }
 
     fileNames.forEach((fullName) => {
         fs.unlinkSync(fullName);
     });
 };
+
+let getPartition = (timestamp) => {
+    let date = new Date(timestamp);
+    return parseInt(date.getMinutes() / 5);
+}
+
+let frameFileName = (timestamp) => {
+    let timeInMinutes = parseInt(timestamp / 1000);
+    return timeInMinutes + ".txt";
+}
